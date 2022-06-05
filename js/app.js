0f48aa849e8192a9e03b2bd11f6a2c9094185649
Anton Pylypenko <90262463+Antonnfs@users.noreply.github.com>
2022-06-06 02:29:45 +0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,15 +1,15 @@
 "use strict"
 
-let a = ""; //first number
-let b = ""; // second number
-let sign = ""; // operation sign
-let finish = false;
+let a = "";	//first number
+let b = "";	// second number
+let sign = "";	// operation sign
+let finish = false;	//equal flag
+let memory = ""; 	// Memory
 
-const digit = [
-	"0",	"00",	"1",	"2",	"3",	"4",	"5",	"6",	"7",	"8",	"9",	".",	"π",	"e", "Rand",
-];
-const action = ["-", "+", "x", "÷", "+/-", "%"];
-const out = document.querySelector(".screen-calc p");
+const digit = ["0", "00", "1", "2",	"3", "4", "5", "6", "7", "8", "9", ".", "π", "e", "Rand", ];
+const action = ["-", "+", "x", "÷", "%", "x!", "x2", "x3", "xy", "yx", "ex", "2x", "10x", "1/x", "2√x", "3√x", "y√x", "ln", "log2", "log10", "logy", "EE", "sin", "cos", "tan", "sinh", "cosh", "tanh", "sin-1", "cos-1", "tan-1", "sinh-1", "cosh-1", "tanh-1"];
+const out = document.querySelector(".screen-calc p");   //output screen
+const type = document.querySelector(".type");	// Rad/Deg
 
 window.addEventListener("click", function (e) {
 	if (!e.target.closest(".button")) return;
@@ -18,43 +18,57 @@ window.addEventListener("click", function (e) {
 	}
 	out.textContent = 0;
 	let prop = e.target.textContent;
-  	// Digit - клавиши 0-9, π, e ...
+	//========================================================================================================================================================
+	if (prop === "+/-") {	// Change the sign
+		a = -a;
+		b = -b;
+		out.textContent = a, b;
+	}
+  	// Digit - Numbers 0-9, Constant π, e ...
 	if (digit.includes(prop)) {
 		if (prop === "π") {
-			console.log(typeof prop);
-			if (a === "" && b === ""  ) {
+			if (a === "" && b === "") {
 				a = Math.PI.toFixed(8);
 				prop = "";
 				finish = false;
 				out.textContent = a;
-			} else if (a !== "") {
+			} else if (a !== "" && sign === "") {
+				a = Math.PI.toFixed(8);
+				prop = "";
+				finish = false;
+				out.textContent = a;
+			} else if (a !== "" && sign !== "") {
 				b = Math.PI.toFixed(8);
 				prop = "";
 				finish = false;
 				out.textContent = b;
 			} else if (a !== "" && b !== "") {
 				prop = "";
-				a = +a + +b;
+				a = calculate();
 				b = Math.PI.toFixed(8);
 				finish = true;
 				out.textContent = a, b;
 			}
 		}
 		if (prop === "e") {
-			console.log(typeof prop);
-			if (a === "" && b === ""  ) {
+			if (a === "" && b === "" ) {
 				a = Math.E.toFixed(8);
 				prop = "";
 				finish = false;
 				out.textContent = a;
-			} else if (a !== "") {
+			} else if (a !== "" && sign === "") {
+				a = Math.E.toFixed(8);
+				prop = "";
+				finish = false;
+				out.textContent = a;
+			} else if (a !== "" && sign !== "") {
 				b = Math.E.toFixed(8);
 				prop = "";
 				finish = false;
 				out.textContent = b;
-			} else if (a !== "" && b !== "") {
+			} else {
 				prop = "";
-				a = +a + +b;
+				a = calculate();
 				b = Math.E.toFixed(8);
 				finish = true;
 				out.textContent = a, b;
@@ -64,14 +78,22 @@ window.addEventListener("click", function (e) {
 			if (a === "" && b === "") {
 				a = Math.random().toFixed(8);
 				prop = "";
+				finish = false;
 				out.textContent = a;
-			} else if (a !== "" && b === "") {
+			} else if (a !== "" && sign === "") {
+				a = Math.random().toFixed(8);
+				prop = "";
+				finish = false;
+				out.textContent = a;
+			} else if (a !== "" && sign !== "") {
 				b = Math.random().toFixed(8);
 				prop = "";
+				finish = false;
 				out.textContent = b;
-			} else {
+			}  else {
 				prop = "";
 				a = calculate();
+				finish = true;
 				b = Math.random().toFixed(8);
 				out.textContent = b;
 			}
@@ -87,65 +109,257 @@ window.addEventListener("click", function (e) {
 			b += prop;
 			out.textContent = b;
 		}
-		console.log(a, b, sign);
+		console.log(a, sign, b);
 		return;
 	}
-	// если нажата клавиша +, -, /, * ...
+	//========================================================================================================================================================
+	// Actions +, -, /, * ...
 	if (action.includes(prop)) {
 		sign = prop;
-		if (sign === "+/-") {
-			a = -a;
-			b = -b;
-			out.textContent = a, b;
-		} else if (sign === "%") {
+		if (sign === "%") {
 			if (a !== "" && b !== "" && sign !== "") {
 				a = calculate();
+				finish = true;
 				console.log(a);
-				a = a / 100;
 				out.textContent = a;
 			}
 		a = calculate();
       a = a / 100;
+		finish = true;
       out.textContent = a;
+		} else if (sign === "x!") {
+				a = factorial(a);
+				finish = true;
+				out.textContent = a;
+				console.log(a);
 		} else if (a !== "" && b !== "") {
-			
+			a = calculate();
+			b = "";
+			finish = true;
+			out.textContent = prop;
 		} else {
       out.textContent = sign;
 		}
-		console.log(a, b, sign);
+		console.log(a, sign, b );
 		return;
 	}
-  // если нажат =
+  	// если нажат =
 	if (prop === "=") {
 		if (b === "") b = a;
 		if (a === "") a = 0;
-		calculate();
+		a = calculate();
+		b = "";
 		finish = true;
 		out.textContent = a;
 		console.log(a, b, sign);
 	}
-  	// togge 2nd button
-	toggle2nd(e);
+	addBrace(e);
+	calcMemory(e);  // Memory
+	toggleType(e);  // Rad/Deg
+	toggle2nd(e);   // togge 2nd button
 });
-
 function calculate() {
 	switch (sign) {
 		case "+":
-			a = +a + +b;
-			break;
+			a = parseFloat((+a + +b).toFixed(8).toString());
+		break;
 		case "-":
-			a = a - b;
-			break;
+			a = parseFloat((a - b).toFixed(8).toString());
+		break;
 		case "x":
-			a = a * b;
-			break;
+			a = parseFloat((a * b).toFixed(8).toString());
+		break;
 		case "÷":
 			if (b === "0" || b === "00") {
-			divByZero();
+				divByZero().out.textContent;
 			return;
-		}
-		a = a / b;
+			}
+			a = parseFloat((a / b).toFixed(8).toString());
 		break;
+		case "x2":
+			a = parseFloat((Math.pow(a, 2)).toFixed(8).toString());
+		break;
+		case "x3":
+			a = parseFloat((Math.pow(a, 3)).toFixed(8).toString());
+		break; 
+		case "xy":
+			a = parseFloat((Math.pow(a, b)).toFixed(8).toString());
+		break;  
+		case "yx":
+			a = parseFloat((Math.pow(b, a)).toFixed(8).toString());
+		break; 
+		case "ex":
+			a = parseFloat((Math.pow(Math.E.toFixed(8), a)).toFixed(8).toString());
+		break; 
+		case "10x":
+			a = parseFloat((Math.pow(10, a)).toFixed(8).toString());
+		break; 
+		case "2x":
+			a = parseFloat((Math.pow(2, a)).toFixed(8).toString());
+		break; 
+		case "1/x":
+			a = parseFloat((Math.pow(a, -1)).toFixed(8).toString());
+		break; 
+		case "2√x":
+			a = parseFloat((Math.sqrt(a)).toFixed(8).toString());
+		break; 
+		case "3√x":
+			a = parseFloat((Math.cbrt(a)).toFixed(8).toString());
+		break; 
+		case "y√x":
+			if (a === "0" || b === "0") {
+				return error().out.textContent;
+			} else if (a === "00" || b === "00") {
+				return error().out.textContent;
+			}
+			a = parseFloat((Math.pow(a, 1 / b)).toFixed(8).toString());
+		break; 
+		case "ln":
+			a = parseFloat((Math.log(a)).toFixed(8).toString());
+			console.log(a);
+		break; 
+		case "log10":
+			a = parseFloat((Math.log10(a)).toFixed(8).toString());
+			console.log(a);  
+		break; 
+		case "logy":
+			a = parseFloat((Math.log(a) / Math.log(b)).toFixed(8).toString());
+			console.log(a);  
+		break; 
+		case "log2":
+			a = parseFloat((Math.log2(a)).toFixed(8).toString());
+			console.log(a);  
+		break;  
+		case "EE":
+			a = parseFloat((a * Math.pow(10, b)).toFixed(8).toString());
+		break; 
+	}
+	calculateSinCos();
+	finish = true;
+	console.log(a);
+	return a;
+}
+function calculateSinCos() {
+	if (type.classList.contains("rad")) {
+		switch(sign) {
+			//Radian
+			case "sin":  
+			a = parseFloat((Math.sin(a)).toFixed(8).toString());
+			break;
+			case "cos":  
+				a = parseFloat((Math.cos(a)).toFixed(8).toString());
+			break;
+			case "tan":  
+				a = parseFloat((Math.tan(a)).toFixed(8).toString());
+			break;
+			case "sinh":  
+				a = parseFloat((Math.sinh(a)).toFixed(8).toString());
+			break;
+			case "cosh":  
+				a = parseFloat((Math.cosh(a)).toFixed(8).toString());
+			break;
+			case "tanh":  
+				a = parseFloat((Math.tanh(a)).toFixed(8).toString());
+			break;
+			// Rad Arc
+			case "sin-1":  
+				a = parseFloat((Math.asin(a)).toFixed(8).toString());
+			break;
+			case "cos-1":  
+				a = parseFloat((Math.acos(a)).toFixed(8).toString());
+			break;
+			case "tan-1":  
+				a = parseFloat((Math.atan(a)).toFixed(8).toString());
+			break;
+			case "sinh-1":  
+				a = parseFloat((Math.asinh(a)).toFixed(8).toString());
+			break;
+			case "cosh-1":  
+				a = parseFloat((Math.acosh(a)).toFixed(8).toString());
+			break;
+			case "tanh-1":  
+				a = parseFloat((Math.atanh(a)).toFixed(8).toString());
+			break;
+		}
+		finish = true;
+		console.log(a);
+		return a;
+	} else {
+		switch(sign) {
+			//Degrees
+			case "sin":  
+				a = parseFloat(((Math.sin(a)) / 180 * Math.PI).toFixed(8).toString());
+			break;
+			case "cos":  
+				a = parseFloat((Math.cos(a)).toFixed(8).toString());
+			break;
+			case "tan":  
+				a = parseFloat((Math.tan(a)).toFixed(8).toString());
+			break;
+			case "sinh":  
+				a = parseFloat((Math.sinh(a)).toFixed(8).toString());
+			break;
+			case "cosh":  
+				a = parseFloat((Math.cosh(a)).toFixed(8).toString());
+			break;
+			case "tanh":  
+				a = parseFloat((Math.tanh(a)).toFixed(8).toString());
+			break;
+			// Deg Arc
+			case "sin-1":  
+				a = parseFloat((Math.asin(a)).toFixed(8).toString());
+			break;
+			case "cos-1":  
+				a = parseFloat((Math.acos(a)).toFixed(8).toString());
+			break;
+			case "tan-1":  
+				a = parseFloat((Math.atan(a)).toFixed(8).toString());
+			break;
+			case "sinh-1":  
+				a = parseFloat((Math.asinh(a)).toFixed(8).toString());
+			break;
+			case "cosh-1":  
+				a = parseFloat((Math.acosh(a)).toFixed(8).toString());
+			break;
+			case "tanh-1":  
+				a = parseFloat((Math.atanh(a)).toFixed(8).toString());
+			break;
+		}
+		finish = true;
+		console.log(a);
+		return a;
+	}
+}
+function factorial(n) {
+	return n ? n * factorial(n - 1) : 1;
+}
+function calcMemory(e) {		// Memory
+	let mrButton = document.querySelector(".mr");	// Rewrite Memory
+	let mrTarget = e.target.closest(".mr");
+	if (e.target.closest(".mc")) {		// Clean Memory
+		memory = "";	
+		mrButton.classList.remove("action");
+		clearAll();
+	}
+	if (mrTarget) {									
+		a = memory;
+		out.textContent = a;
+	}
+	if (e.target.closest(".m-")) {
+		if (memory !== "") {
+			a = memory - a;
+			return out.textContent = a;
+		}
+	}
+	if (e.target.closest(".m-plus")) {
+		memory = +memory + +a;
+		if (memory !== "") {
+			console.log(`Memory: ${memory}`);
+			mrButton.classList.add("action");
+		} else {
+			mrButton.classList.remove("action");
+		}
+		clearAll();
 	}
 }
 function clearAll() {
@@ -162,6 +376,24 @@ function divByZero() {
 	finish = false;
 	out.textContent = "Делить на ноль нельзя";
 }
+function error() {
+	a = "";
+	b = "";
+	sign = "";
+	finish = false;
+	out.textContent = "Ошибочка";
+}
+function toggleType(e) {		// Rad/Deg
+	if (e.target.closest(".type")) {
+		type.classList.toggle("rad");
+		type.classList.toggle("action");
+		if (type.classList.contains("rad")) {
+			type.textContent = "Deg";
+		} else {
+			type.textContent = "Rad";
+		}
+	}
+}
 function toggle2nd(e) {
 	const firstGroup = document.querySelectorAll(".first-group");
 	const secondGroup = document.querySelectorAll(".second-group");
@@ -176,10 +408,11 @@ function toggle2nd(e) {
 		});
 	}
 }
-function cuttingText() {
-	for(let i = 0; i < out.length; i++) {
-		out[i].innerText = out[i].innerText.slice(0, 30) + '...';
-		if (out.length > 20) out.length === 20;
+function addBrace(e) {
+	if (e.target.closest(".left-brace")) {
+		out.textContent = "(";
 	}
-};
-cuttingText();
+	if (e.target.closest(".right-brace")) {
+		out.textContent = ")";
+	}
+}
