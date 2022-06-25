a055e1f213f40ebd8e3abd85b51d4e7f8e844b50
Anton Pylypenko <90262463+Antonnfs@users.noreply.github.com>
2022-06-25 10:06:16 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 "use strict"
 
-let a = "";	//first number
-let b = "";	// second number
-let sign = "";	// operation sign
+let a = "";		//first number
+let b = "";		// second number
+let sign = "";		// operation sign
 let finish = false;	//equal flag
 let memory = ""; 	// Memory
 
